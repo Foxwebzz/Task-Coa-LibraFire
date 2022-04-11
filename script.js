4641edaff0a9b6929a5f452ad2269a40910833f3
DESKTOP-UG3PI5T\Novakovic <nikola2312017@gmail.com>
2022-04-11 13:55:29 +0200

    responsive and hamburger menu done for mobile

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let hamBtn = document.querySelector('.hamburger-custom')
 let ham1 = document.querySelector('.ham1')
 let ham2 = document.querySelector('.ham2')
 let ham3 = document.querySelector('.ham3')
+let navUlBg = document.querySelector('.navigation-right-side')
+let navUl = document.querySelector('.navigation-ul')
+
 let clicked = false;
 
 hamBtn.addEventListener('click', function() {
@@ -10,6 +13,8 @@ hamBtn.addEventListener('click', function() {
         ham1.style.transform = "rotate(45deg)"
         ham2.style.transform = "rotate(-45deg)"
         ham2.style.marginTop = "-7px"
+
+        navUlBg.style.left = "0"
         
         clicked = true;
     } else if (clicked === true) {
@@ -17,6 +22,8 @@ hamBtn.addEventListener('click', function() {
         ham1.style.transform = "rotate(0)"
         ham2.style.transform = "rotate(0)"
         ham2.style.marginTop = "0"
+
+        navUlBg.style.left = "-200px"
         
         clicked = false;
     }
